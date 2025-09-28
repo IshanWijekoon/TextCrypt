b8a5e7b1b4c91f39c069efb8358d9fde9aba02f5
Ishan Wijekoon <ishancoc295@gmail.com>
2025-09-28 21:02:27 +0530

    added some changes

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 text = 'Hello Zaira'
 custom_key = 'python'
 
@@ -8,15 +7,21 @@ def vigenere(message, key):
     encrypted_text = ''
 
     for char in message.lower():
-         # Append space to the message
+    
+        # Append space to the message
         if char == ' ':
             encrypted_text += char
-        else:
-            
+        else:        
+            # Find the right key character to encode
             key_char = key[key_index % len(key)]
             key_index += 1
+
+            # Define the offset and the encrypted letter
+            offset = alphabet.index(key_char)
             index = alphabet.find(char)
             new_index = (index + offset) % len(alphabet)
             encrypted_text += alphabet[new_index]
-print('plain text:', message)
-print('encrypted text:', encrypted_text)
+    
+    return encrypted_text
+
+encryption = vigenere(text, custom_key)
